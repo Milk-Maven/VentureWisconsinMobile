d6c009102b8a07159d355607f2cfc1ed39a51e73
Milk-Maven <tylerf66@gmail.com>
2023-02-22 11:54:55 -0600

    Cleaned up routing

--- App.tsx
+++ App.tsx
@@ -4,8 +4,27 @@ import { RecoilRoot } from "recoil";
 import { t } from "./providers/providers";
 import { httpBatchLink } from "@trpc/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Routing } from "./components/Routing";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { ROUTES } from "./utils/consts";
+import { AdminPage } from "./pages/Admin/AdminPage";
+import { ListingPage } from "./pages/Listing/ListingPage";
+import { UserPage } from "./pages/User/UserPage";
+import { BottomNavigation } from "./components/BottomNavigation";
+import { needsToLoginOrCreateAccount, UserSession } from "./providers/Auth";
+import { CreateNewUserPage } from "./pages/Login/CreateNewUserPage";
 export default function App() {
+  const Stack = createNativeStackNavigator();
+  const [session, setUserSession] = useState<UserSession>({
+    email: null,
+    session: null,
+  });
+  useEffect(() => {
+    needsToLoginOrCreateAccount().then((user) => {
+      console.log(user);
+      setUserSession(user);
+    });
+  }, []);
   const [queryClient] = useState(() => new QueryClient());
   const [trpcClient] = useState(() =>
     t.createClient({
@@ -21,7 +40,39 @@ export default function App() {
     <t.Provider client={trpcClient} queryClient={queryClient}>
       <QueryClientProvider client={queryClient}>
         <RecoilRoot>
-          <Routing></Routing>
+          <NavigationContainer>
+            <Stack.Navigator>
+              {session.session && (
+                <>
+                  <Stack.Screen
+                    name={ROUTES.ADMIN_PAGE}
+                    component={AdminPage}
+                    options={{ title: "Admin", headerShown: false }}
+                  />
+                  <Stack.Screen
+                    name={ROUTES.LISTING_PAGE}
+                    component={ListingPage}
+                    options={{ title: "Main", headerShown: false }}
+                  />
+                  <Stack.Screen
+                    name={ROUTES.USER_PAGE}
+                    component={UserPage}
+                    options={{ title: "User", headerShown: false }}
+                  />
+                </>
+              )}
+              {!session.session && (
+                <>
+                  <Stack.Screen
+                    name={ROUTES.CREATE_NEW_USER_PAGE}
+                    component={CreateNewUserPage}
+                    options={{ title: "Create Account", headerShown: false }}
+                  />
+                </>
+              )}
+            </Stack.Navigator>
+            <BottomNavigation />
+          </NavigationContainer>
         </RecoilRoot>
       </QueryClientProvider>
     </t.Provider>
